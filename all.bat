f0faeb7068a7816b9834a0ca1b27656e49b5cb5b
Revangerrrr <pervakov92@gmail.com>
2023-03-22 03:40:51 +0700

    example

---
+++ all.bat
@@ -0,0 +1,15 @@
+g++ -c ./server/source/server.cpp -o ./server/build/server.o
+g++ -c ./server/source/iserver.cpp -o ./server/build/iserver.o
+
+xcopy .\server\build\server.o .\client\build\lib /Y
+xcopy .\server\build\iserver.o .\client\build\lib /Y
+xcopy .\server\source\iserver.h .\client\source\lib /Y
+
+g++ -c ./client/source/client.cpp  -o ./client/build/client.o
+g++ -c ./client/source/wrapper.cpp -o ./client/build/wrapper.o
+
+g++ ./client/build/client.o ./client/build/wrapper.o ./client/build/lib/iserver.o ./client/build/lib/server.o    -o ./client/build/client.exe
+
+"client/build/client.exe"
+
+pause
